8f5f8007d545dacfc9af3b26903b5c0f423c4ded
Banana <banana.computermonkey@gmail.com>
2020-07-12 16:06:11 -0700

    working on ABCD buttons

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 var startButton = document.getElementById('start-btn');
 var questionText= document.getElementById('question');
 var questionIndex = 0
+var buttonAText = document.getElementById("btn-A");
+
+
 
 startButton.addEventListener('click', startGame);
+
+
+
+
+
 const questionArray = [
     {
     question: 'What is 2 = 2',
     answers: [
-        { text: 'A: 4', correct: true},
-        { text: 'B: 17', correct: false},
-        { text: 'A: 4', correct: true},
-        { text: 'A: 4', correct: true}
+        { textA: 'A: 4', correct: true},
+        { textB: 'B: 17', correct: false},
+        { textC: 'A: 4', correct: true},
+        { textD: 'A: 4', correct: true}
     ] },
     
     // {
@@ -36,19 +44,25 @@ const questionArray = [
 function startGame () {
     nextQuestion()
     timer()
+//     buttonAText.addEventListener('click', nextQuestion);
+// console.log(buttonAText)
 }
 
 function nextQuestion () {
-    var question=questionArray[questionIndex].question
-    questionText.innerHTML=question
+    var question=questionArray[questionIndex].question;
+    questionText.innerHTML=question;
+    var buttonA=questionArray[questionIndex].answers.textA;
+    buttonAText.innerHTML=buttonA;
+    console.log(buttonA)
     
 }
 
-console.log(question)
-function selectAnswer () {
+// console.log(question)
+// console.log(buttonA)
+// function selectAnswer () {
+// alert(questionArray.textA)
 
-
-}
+// }
 
 
 var timeTotal= 3;
